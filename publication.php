8e7b74f6c74ecd0f03fbbd1a909e3d23dd549d74
Administrator <admin@example.com>
2014-04-02 20:39:32 +0200

    hamonisation du site et clean css and pages

--- publication.php
+++ publication.php
@@ -1,57 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <meta charset="UTF-8">
-  <title>vCard Walid HASSANI</title>
-
+  <title>Walid HASSANI</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/styleglobal.css">
-
 </head>
+
 <body>
 
-
-
-
-
-
-
-<div class="panel panel-default" style ="width: 1012px;
-    margin-left: 94px;margin-top:48px;">
-  <div class="panel-heading">
-    <h3 class="panel-title">
- <div class="bs-example">
-    <ul class="nav nav-pills">
-        <li ><a href="index.php"><span class="glyphicon glyphicon-home"></span>  About</a></li>
-        <li><a href="resume.php"><span class="glyphicon glyphicon-user"></span> Resume</a></li>
-        <li class="active"><a href="publication.php"><span class="glyphicon glyphicon-envelope"></span> Publications</a></li>
-        <li><a href="contact.php"><span class="glyphicon glyphicon-envelope"></span> Contact</a></li>
-    </ul>
-</div>
-    	
-
-
+  <div class="panel panel-default"  id ="panel_default">
+    <div class="panel-heading">
+      <h3 class="panel-title">
+       <div class="bs-example">
+        <ul class="nav nav-pills">
+          <li >
+            <a href="index.php">
+              <span class="glyphicon glyphicon-home"></span>  About
+            </a>
+          </li>
+          <li>
+            <a href="resume.php">
+              <span class="glyphicon glyphicon-user"></span> Resume
+            </a>
+          </li>
+          <li class="active">
+            <a href="publication.php"><span class="glyphicon glyphicon-envelope"></span> Publications
+            </a>
+          </li>
+          <li>
+            <a href="contact.php"><span class="glyphicon glyphicon-envelope"></span> Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </h3>
   </div>
+  
   <div class="panel-body">
-   <div class="tab-pane"  style="margin-top:38px;" >
-          <div  >
-          </div>
-          <object data="biblio/bibhtml.html" height="1000px" width="100%" type="text/html">
-          </object>
+      <div class="tab-pane" id ="tab_pane" >
+             <object data="biblio/bibhtml.html" height="1000px" width="100%" type="text/html">
+             </object>
 
- </div>
+      </div>
   </div>
 </div>
-
-
-
-
-
-
-
-
-
-    </body>
-    </html>                                               
+</body>
+</html>                                               
